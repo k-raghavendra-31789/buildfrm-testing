6f4b87ff21cbc270ad8aa8a5b476463c466734c7
k-raghavendra-31789 <120781826+k-raghavendra-31789@users.noreply.github.com>
2026-07-17 15:56:11 +0530

    Add Unity Catalog setup notebook from Databricks

---
+++ Sample-notebook.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# COMMAND ----------
+%sql
+CREATE CATALOG IF NOT EXISTS traffic_lakehouse;
+CREATE SCHEMA IF NOT EXISTS traffic_lakehouse.bronze;
+CREATE SCHEMA IF NOT EXISTS traffic_lakehouse.silver;
+
+SHOW CATALOGS LIKE 'traffic_lakehouse';
+SHOW SCHEMAS IN traffic_lakehouse LIKE 'bronze';
+SHOW SCHEMAS IN traffic_lakehouse LIKE 'silver';
